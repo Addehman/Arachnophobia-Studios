ef4933a9a85da34f78977f38e170076af12e08db
Chris <70510078+cseehuusen@users.noreply.github.com>
2021-04-14 13:27:53 +0200

    Added_Inventory_Added_Drop

---
+++ SpiderGame/Assets/Scripts/InventoryThings/ItemDatabase.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    private void Awake()
+    {
+        BuildDatabase();
+    }
+
+    public Item GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    public Item GetItem(string itemName)
+    {
+        return items.Find(item => item.title == itemName);
+
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item>()
+        {
+           new Item(0, "Tomato", "Deli", new Dictionary<string, int>
+           {
+               {"Flavor", 5},
+               {"Vitamin C", 1}
+           }),
+
+           new Item(1, "Banana", "Juicy", new Dictionary<string, int>
+           {
+               {"Flavor", 3},
+               {"Potasium", 1},
+               {"Bananaflies",2},
+               {"Protein",1}
+           }),
+
+           new Item(2, "Blueberry", "Blue", new Dictionary<string, int>
+           {
+               {"Flavor", 3},
+               {"Vitamin C", 1},
+               {"Snails",1}
+           }),
+
+           new Item(3, "Apple", "Blue", new Dictionary<string, int>
+           {
+               {"Flavor", 3},
+               {"Vitamin D", 1},
+               {"Snails",1}
+           })
+        };
+    }
+}
