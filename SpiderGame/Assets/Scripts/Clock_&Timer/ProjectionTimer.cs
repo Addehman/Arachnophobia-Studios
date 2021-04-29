4ce142e134f70b24d512cd336462d4a41366e52c
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2021-04-29 16:07:10 +0200

    Added ProjectionTimer script to indicate when Eric sees the player.

---
+++ SpiderGame/Assets/Scripts/Clock_&Timer/ProjectionTimer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectionTimer : MonoBehaviour
+{
+    Projector proj;
+    Color myColor;
+    float colorIncreaseValue = 0.33f;
+    float increaseValue = 0.05f; //Use 20 for proj.fieldOfView
+
+    private void Awake()
+    {
+        proj = GetComponent<Projector>();
+    }
+
+    void Update()
+    {
+        if (proj.orthographicSize < 0.15)
+        {
+            proj.material.color = myColor;
+
+            myColor.r += colorIncreaseValue * Time.deltaTime;
+
+            proj.orthographicSize += increaseValue * Time.deltaTime;
+        }
+    }
+    
+    private void OnDisable()
+    {
+        proj.orthographicSize = 0.01f;
+        myColor = new Color(0, 0, 0, 1);
+    }
+}
