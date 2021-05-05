3ad14a06e500cb0586f537baa4e5b8f26f39856a
Adam Forsell <adamforsell2@hotmail.com>
2021-05-05 10:56:03 +0200

    Merge branch 'adam/Cinemachine+movement' into main

--- SpiderGame/Assets/Samples/Cinemachine/2.7.3/Cinemachine Example Scenes/Scenes/3rdPersonWithAimMode/ActivateOnKeypress.cs
+++ SpiderGame/Assets/Samples/Cinemachine/2.7.3/Cinemachine Example Scenes/Scenes/3rdPersonWithAimMode/ActivateOnKeypress.cs
@@ -1,38 +1,49 @@
 ﻿using UnityEngine;
+using System;
 
 public class ActivateOnKeypress : MonoBehaviour
 {
-    public KeyCode ActivationKey = KeyCode.LeftControl;
-    public int PriorityBoostAmount = 10;
-    public GameObject Reticle;
+	public event Action<bool> ActivationFPSCam;
 
-    Cinemachine.CinemachineVirtualCameraBase vcam;
-    bool boosted = false;
+	public KeyCode ActivationKey = KeyCode.LeftControl;
+	public int PriorityBoostAmount = 10;
+	public GameObject Reticle;
 
-    void Start()
-    {
-        vcam = GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
-    }
+	Cinemachine.CinemachineVirtualCameraBase vcam;
+	bool boosted = false;
 
-    void Update()
-    {
-        if (vcam != null)
-        {
-            if (Input.GetKey(ActivationKey))
-            {
-                if (!boosted)
-                {
-                    vcam.Priority += PriorityBoostAmount;
-                    boosted = true;
-                }
-            }
-            else if (boosted)
-            {
-                vcam.Priority -= PriorityBoostAmount;
-                boosted = false;
-            }
-        }
-        if (Reticle != null)
-            Reticle.SetActive(boosted);
-    }
+	void Start()
+	{
+		vcam = GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
+	}
+
+	void Update()
+	{
+		if (vcam != null)
+		{
+			if (Input.GetKey(ActivationKey))
+			{
+				if (!boosted)
+				{
+					vcam.Priority += PriorityBoostAmount;
+					boosted = true;
+					if (ActivationFPSCam != null)
+					{
+						ActivationFPSCam(true);
+					}
+				}
+			}
+			else if (boosted)
+			{
+				vcam.Priority -= PriorityBoostAmount;
+				boosted = false;
+				if (ActivationFPSCam != null)
+					{
+						ActivationFPSCam(false);
+					}
+			}
+		}
+		if (Reticle != null)
+			Reticle.SetActive(boosted);
+	}
 }
