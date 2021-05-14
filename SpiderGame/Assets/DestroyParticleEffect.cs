acef7c323bd0ea3eaedf32ef51b666147d80b51f
Chris <70510078+cseehuusen@users.noreply.github.com>
2021-05-14 10:57:53 +0200

    Added ParticleSystem on Ingrediens - take away when pciekd up

---
+++ SpiderGame/Assets/DestroyParticleEffect.cs
@@ -0,0 +1,13 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleEffect : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        transform.GetChild(0).GetComponent<ParticleTakeAway>().TakeAwayThisParticleEffect();
+        Debug.Log("DestroyParticleEffect");
+    }
+}
+*/
---
+++ SpiderGame/Assets/ParticleTakeAway.cs
@@ -0,0 +1,13 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTakeAway : MonoBehaviour
+{
+    public void TakeAwayThisParticleEffect()
+    {
+        Destroy(gameObject);
+        Debug.Log("ParticleTakeAway");
+    }
+}
+*/
