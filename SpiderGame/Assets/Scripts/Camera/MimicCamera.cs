ebb0a2552c9adeba76005cffb390fdc375fccfd5
Adam Forsell <adamforsell2@hotmail.com>
2021-05-19 17:05:35 +0200

    fixed hookShot camera movement

--- SpiderGame/Assets/Scripts/Camera/MimicCamera.cs
+++ SpiderGame/Assets/Scripts/Camera/MimicCamera.cs
@@ -7,12 +7,40 @@ public class MimicCamera : MonoBehaviour
 	[SerializeField] private float positionLerpSpeed = 10f;
 	[SerializeField] private float rotationLerpSpeed = 10f;
 
-	private Vector3 currentVelocity;
+	public bool doLockRotation = false;
 
+	private Vector3 currentVelocity;
+	private Quaternion lockedRotation;
+	private HookWeb hookWeb;
+
+
+	private void Start()
+	{
+		hookWeb = FindObjectOfType<HookWeb>();
+		hookWeb.LockTPCameraRotation += LockRotation;
+	}
 
 	private void LateUpdate()
 	{
 		transform.position = Vector3.SmoothDamp(transform.position, cameraToMimic.position, ref currentVelocity, positionLerpSpeed * Time.deltaTime);
-		transform.rotation = Quaternion.Slerp(transform.rotation, cameraToMimic.rotation, rotationLerpSpeed * Time.deltaTime);
+
+		if (doLockRotation == true)
+		{
+			transform.rotation = lockedRotation;
+		}
+		else
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, cameraToMimic.rotation, rotationLerpSpeed * Time.deltaTime);
+		}
+	}
+
+	public void LockRotation(bool isActive)
+	{
+		if (isActive == true)
+		{
+			lockedRotation = transform.rotation;
+		}
+
+		doLockRotation = isActive;
 	}
 }
