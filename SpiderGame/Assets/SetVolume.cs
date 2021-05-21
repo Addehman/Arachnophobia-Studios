249ef270d0b4b5a5d5de14c3e872fa62eacfc5e3
cseehuusen <70510078+cseehuusen@users.noreply.github.com>
2021-05-21 11:33:05 +0200

    Mixer x3, 2 Scripts for menu, graphics settings

---
+++ SpiderGame/Assets/SetVolume.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer AudioMixer;
+    public Slider slider;
+
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        sliderValue = Mathf.Clamp(sliderValue, 0.0001f, 1);
+        AudioMixer.SetFloat("Master", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+}
---
+++ SpiderGame/Assets/SettingsMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
