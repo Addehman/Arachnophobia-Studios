2417947bced6dc341dce4fa66638bbd83c533b5b
TosaLipkiss <tovahl2002@skola.goteborg.se>
2021-05-26 11:58:16 +0100

    Cutscene and introscene implemented

---
+++ SpiderGame/Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour
+{
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 75f)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
