08f19bcfc644b9b7085c674b8b6e18c5f8987faf
Adam Forsell <adamforsell2@hotmail.com>
2021-05-27 16:22:04 +0200

    fixed UI ingredients/items! Overriding all prefabs to make sure everything is saved!

--- SpiderGame/Assets/Scripts/Inventory/UIInventory.cs
+++ SpiderGame/Assets/Scripts/Inventory/UIInventory.cs
@@ -5,56 +5,55 @@ using UnityEngine;
 
 public class UIInventory : MonoBehaviour
 {
-    public List<UIItem> uIItems = new List<UIItem>();
-    public GameObject slotPrefab;
-    public Transform slotPanel;
-    public int numberOfSlots = 9; 
+	public List<UIItem> uIItems = new List<UIItem>();
+	public GameObject slotPrefab;
+	public Transform slotPanel;
+	public int numberOfSlots = 9; 
 
-    private void Awake()
-    {
-        uIItems = new List<UIItem>();
+	private void Awake()
+	{
+		uIItems = new List<UIItem>();
 
-        InstantiateSlotPrefab();
+		InstantiateSlotPrefab();
 
-    }
+	}
 
-    public void InstantiateSlotPrefab()
-    {
-        for (int i = 0; i < numberOfSlots; i++)
-        {
-            GameObject instance = Instantiate(slotPrefab);
-            instance.transform.SetParent(slotPanel);
-            uIItems.Add(instance.GetComponentInChildren<UIItem>());
-        }
-    }
+	public void InstantiateSlotPrefab()
+	{
+		for (int i = 0; i < numberOfSlots; i++)
+		{
+			UIItem item = Instantiate(slotPrefab, slotPanel).GetComponentInChildren<UIItem>();
+			uIItems.Add(item);
+		}
+	}
 
-    public void UpdateSlot(int slot, ItemInfo item)
-    {
-        uIItems[slot].UpdateItem(item);
-    }
+	public void UpdateSlot(int slot, ItemInfo item)
+	{
+		uIItems[slot].UpdateItem(item);
+	}
 
-    public void AddNewItem(ItemInfo item)
-    {
-        foreach (var uIItem in uIItems)
-        {
-            if (uIItem.item == null)
-            {
-                uIItem.UpdateItem(item);
-                break;
-            }
-        }
-    }
+	public void AddNewItem(ItemInfo item)
+	{
+		foreach (var uIItem in uIItems)
+		{
+			if (uIItem.item == null)
+			{
+				uIItem.UpdateItem(item);
+				break;
+			}
+		}
+	}
 
-    public void RemoveItem(ItemInfo item)
-    {
-        foreach (var uIItem in uIItems)
-        {
-            if (uIItem.item == item)
-            {
-                uIItem.UpdateItem(item);
-                break;
-            }
-        }
-    }
+	public void RemoveItem(ItemInfo item)
+	{
+		foreach (var uIItem in uIItems)
+		{
+			if (uIItem.item == item)
+			{
+				uIItem.UpdateItem(item);
+				break;
+			}
+		}
+	}
 }
 
